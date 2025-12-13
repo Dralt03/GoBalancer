4bcd84138f651d67cf4b90d859a7a0463c8a4a05
dralt03 <sharmasaurenb@gmail.com>
2025-12-13 18:18:37 +0530

    Feat: Added least Connection balancer

--- internal/balancer/leastconn.go
+++ internal/balancer/leastconn.go
@@ -1,1 +1,36 @@
 package balancer
+
+import (
+	"LoadBalancer/internal/backend"
+	"errors"
+)
+
+type LeastConnections struct {
+	pool *backend.Pool
+}
+
+func NewLeastConnectionsBalancer(pool *backend.Pool) *LeastConnections{
+	return &LeastConnections{
+		pool: pool,
+	}
+}
+
+func (lc *LeastConnections) Pick() (*backend.Backend, error){
+	backends := lc.pool.AliveSnapshot()
+	n := len(backends)
+	if n == 0 {
+		return nil, errors.New("no alive backends")
+	}
+
+	selected := backends[0]
+	minCount := selected.ConnCount()
+	for _, b := range backends {
+		currCount := b.ConnCount()
+		if currCount < minCount {
+			selected = b
+			minCount = currCount
+		}
+	}
+	selected.IncConn()
+	return selected, nil
+}
